1f59edeb0a35e2c88e0bce726c66d132ba047d11
mm667937 <marketa.muzikova@broadcom.com>
2023-11-30 15:58:59 +0100

    redirect

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,7 @@ let title = document.querySelector("#my_title");
 let btn = document.querySelector("#myButon");
 btn.addEventListener('click', async function () {
     //alert(my_textbox.value);
-    const bla = await fetch("https://account-d.docusign.com/oauth/auth?response_type=token&scope=signature&client_id=6fae2312-7c3f-454a-969b-490247d8f80e",{
+    const bla = await fetch("https://account-d.docusign.com/oauth/auth?response_type=token&scope=signature&client_id=6fae2312-7c3f-454a-969b-490247d8f80e&redirect_uri=https://muzikovam.github.io/zowe.github.io/form.html",{
         method: "GET",
         headers: {
             "Content-Type": "application/json",
