2068c7a4c4415f6119e32ab9d51d9b1647377397
mm667937 <marketa.muzikova@broadcom.com>
2023-11-30 15:41:12 +0100

    new id

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,7 @@ let title = document.querySelector("#my_title");
 let btn = document.querySelector("#myButon");
 btn.addEventListener('click', async function () {
     //alert(my_textbox.value);
-    const bla = await fetch("https://account-d.docusign.com/oauth/auth?response_type=token&scope=signature&client_id=43c63a23-b847-41f3-bdea-aee2bf86bf44",{
+    const bla = await fetch("https://account-d.docusign.com/oauth/auth?response_type=token&scope=signature&client_id=6fae2312-7c3f-454a-969b-490247d8f80e",{
         method: "GET",
     });
     console.log(bla);
