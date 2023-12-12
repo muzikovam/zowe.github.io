968a758bcac0e20dc58c7eccaaef54d1bf9e3476
mm667937 <marketa.muzikova@broadcom.com>
2023-12-12 16:43:26 +0100

    trying github env variable

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,7 @@ let harmonica= document.querySelector("#harmonica");
 let title = document.querySelector("#my_title");
 let btn = document.querySelector("#myButon");
 btn.addEventListener('click', async function () {
+    alert(process.env.GH_SECRET);
     //alert(my_textbox.value);
     const bla = await fetch("https://account-d.docusign.com/oauth/auth?response_type=token&scope=signature&client_id=6fae2312-7c3f-454a-969b-490247d8f80e&redirect_uri=https://muzikovam.github.io/zowe.github.io/form.html",{
         method: "GET",
@@ -16,4 +17,9 @@ btn.addEventListener('click', async function () {
         },
     });
     console.log(bla);
+    var creds = JSON.stringify({
+  Username: "muzikovam@gmail.com",
+  Password: "password",
+  IntegratorKey: "integratorKey"
+});
 });
